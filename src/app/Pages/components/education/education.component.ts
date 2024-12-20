906f6ed0c72eda6166b5a69a86cd2956f408ef7b
Anand Acharya <anandacharyadx3555@gmail.com>
2024-12-20 14:37:45 -0600

    update code

--- src/app/Pages/components/education/education.component.ts
+++ src/app/Pages/components/education/education.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { experiencesEn} from '../education/experiencesEn';
+import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-education',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './education.component.html',
     styleUrl: './education.component.css'
 })
 export class EducationComponent {
-
+    experiences:any=experiencesEn;
+    constructor() {
+        this.experiences=experiencesEn;
+    }
+    detailOnClick(experience: any) {
+        experience.detailIsDisplayed=!experience.detailIsDisplayed;
+      }
 }
---
+++ src/app/Pages/components/education/experiencesEn.ts
@@ -0,0 +1,46 @@
+export const experiencesEn = [
+    {
+        title: "Bachelor of Engineering",
+        company: 'Gujarat Technological University- Ahmedabad, Gujarat, India',
+        period: "2011 - 2015",
+        technologies: '',
+        missions: [
+            "Computer Engineering"
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Java Developer",
+        company: 'Maxgen Technologies, Ahmedabad, India',
+        period: "2016 - 2017",
+        technologies: 'JAVA 8, J2EE, JSP, Serverlet, Javascript, Ajax, MySql',
+        missions: [
+            "Implemented ERP System and Employee Payroll System Module, which manage employee profile, salary, allowances, and deduction information. Developed full-stack J2EE apps with Java, JSP, JavaScript, Ajax on the frontend, and Servlet, EJB, Hibernate on the backend, integrating MongoDB and MySQL databases.",
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Mobile App Developer",
+        company: 'While Color Web Solutions, India',
+        period: "June 2017 - Jan 2018 ",
+        technologies: 'Ionic, Android, IOS, Bootstrap, Angular, Angular, MySql',
+        missions: [
+            "Built and maintained hybrid e-commerce app using Ionic and Angular, enabling users and admins to manage orders and offers.",
+            "Optimized app performance by identifying and resolving code inefficiencies, improving overall user experience.",
+            ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Ionic Framework and Angular App Developer",
+        company: 'Troot Technologies LLP, India',
+        period: "Feb 2018 - Dec 2018   	 ",
+        technologies: 'NodeJs, Sql, Angular, Ionic, Android, Ios, Sails JS, ExpressJS, MVC, HTML/CSS, JS, Typescript, CouchDB/PouchDB',
+        missions: [
+            "Developed cross-platform educational applications using Ionic and Angular, integrating student, instructor, and admin modules with seamless payment and data tracking.", 
+            "Designed and launched 2 mobile apps and full-stack web app with technologies like Typescript, NodeJS, and Ionic, Angular.",
+
+        ],
+        detailIsDisplayed: false,
+    },
+];
+
